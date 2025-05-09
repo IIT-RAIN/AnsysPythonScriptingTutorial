2d2658ed5e7772c0d4a7cbe8230b226d73fbac26
Ardit Poka <127320779+arditpoka@users.noreply.github.com>
2025-05-09 10:57:06 +0200

    Delete extract_max_stress.py

--- extract_max_stress.py
+++
@@ -1,6 +0,0 @@
-def extract_max_stress(stress_result):
-    """
-    Evaluate and return maximum von Mises stress (Pa).
-    """
-    stress_result.EvaluateAllResults()
-    return stress_result.Maximum
